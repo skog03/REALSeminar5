ce94459c34ad0d895786b6ee00b9bfa9f7049999
RudolfsTaurins <s22sprokate@venta.lv>
2024-04-26 16:08:38 +0300

    added some  crud functions for database

--- src/main/java/lv/venta/service/implementation/ProductServiceImplementation.java
+++ src/main/java/lv/venta/service/implementation/ProductServiceImplementation.java
@@ -2,14 +2,19 @@ package lv.venta.service.implementation;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lv.venta.model.Product;
+import lv.venta.repo.IProductRepo;
 import lv.venta.service.IProductCRUDservice;
 import lv.venta.service.IProductFilteringService;
 
 @Service
 public class ProductServiceImplementation implements IProductCRUDservice, IProductFilteringService{
+	
+	@Autowired
+	private IProductRepo productRepo;
 
 	@Override
 	public ArrayList<Product> filterByQuantityThreshold(int threshold) throws Exception {
@@ -31,25 +36,56 @@ public class ProductServiceImplementation implements IProductCRUDservice, IProdu
 
 	@Override
 	public ArrayList<Product> retrieveAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if(productRepo.count() == 0) {
+			throw new Exception("Table is empty");
+		}
+		ArrayList<Product> result = (ArrayList<Product>)productRepo.findAll();  //called casting/a cast
+		return result;
 	}
 
 	@Override
 	public Product retrieveById(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if(productRepo.count() == 0) {
+			throw new Exception("Table is empty");
+		}
+		
+		if(productRepo.existsById(id)) {
+			return productRepo.findById(id).get();
+		}
+		
+		throw new Exception("Product with id: " + id + " does not exist");
+		
 	}
 
 	@Override
 	public void updateById(int id, String title, String description, float price, int quantity) throws Exception {
-		// TODO Auto-generated method stub
+
+		if(id < 0 || title == null || description == null || price < 0 || quantity < 0) {
+			throw new Exception("Wrong input");
+		}
+		
+		if(productRepo.existsById(id)) {
+			Product productForUpdating = retrieveById(id);
+			productForUpdating.setTitle(title);
+			productForUpdating.setDescription(description);
+			productForUpdating.setPrice(price);
+			productForUpdating.setQuantity(quantity);
+			
+			productRepo.save(productForUpdating);
+		}
 		
 	}
 
 	@Override
 	public void deleteById(int id) throws Exception {
-		// TODO Auto-generated method stub
+
+		if(productRepo.existsById(id)) {
+			Product prodToDelete = retrieveById(id);
+			productRepo.delete(prodToDelete);
+			
+		}
+		
+		
 		
 	}
 
