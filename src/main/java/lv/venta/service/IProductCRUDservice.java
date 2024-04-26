d2837103d5ca55cbbf0fca022aee67903c864fe0
RudolfsTaurins <s22sprokate@venta.lv>
2024-04-26 14:01:05 +0300

    functions added for product

---
+++ src/main/java/lv/venta/service/IProductCRUDservice.java
@@ -0,0 +1,25 @@
+package lv.venta.service;
+
+import java.util.ArrayList;
+
+import lv.venta.model.Product;
+
+public interface IProductCRUDservice {
+	//crud - create retrieve update delete
+	
+	//create
+	public abstract void create(String title, String description, float price, int quantity);
+	
+	//retrieve all
+	public abstract ArrayList<Product> retrieveAll() throws Exception;
+	
+	//retrieve one
+	public abstract Product retrieveById(int id) throws Exception;
+	
+	//update
+	public abstract void updateById(int id, String title, String description, float price, int quantity) throws Exception;
+	
+	//delete
+	public abstract void deleteById(int id) throws Exception;
+
+}
---
+++ src/main/java/lv/venta/service/implementation/ProductServiceImplementation.java
@@ -0,0 +1,5 @@
+package lv.venta.service.implementation;
+
+public class ProductServiceImplementation {
+
+}
